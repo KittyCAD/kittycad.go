434c8561628da7d4dda7929ad61b9abaf95cd4a3
Jess Frazelle <github@jessfraz.com>
2021-12-08 10:34:41 -0800

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- helpers.go
+++ helpers.go
@@ -12,7 +12,7 @@ const KITTYCAD_TOKEN_ENV_VAR = "KITTYCAD_API_TOKEN"
 
 /// NewDefaultClient creates a new client for the KittyCad API.
 /// You need to pass in your API token to create the client.
-func NewDefaultClient(token string) (*Client, error) {
+func NewDefaultClient(token string) (*ClientWithResponses, error) {
 	if token == "" {
 		return nil, fmt.Errorf("You need to pass in an API token to create the client. Create a token at https://kittycad.io/account")
 	}
@@ -22,7 +22,7 @@ func NewDefaultClient(token string) (*Client, error) {
 		return nil, fmt.Errorf("Failed to create security provider: %s", err)
 	}
 
-	client, err := NewClient(KITTYCAD_DEFAULT_SERVER, WithRequestEditorFn(bearerTokenProvider.Intercept))
+	client, err := NewClientWithResponses(KITTYCAD_DEFAULT_SERVER, WithRequestEditorFn(bearerTokenProvider.Intercept))
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create client: %s", err)
 	}
@@ -32,7 +32,7 @@ func NewDefaultClient(token string) (*Client, error) {
 
 // NewDefaultClientFromEnv creates a new client for the KittyCad API, using the token
 // stored in the environment variable `KITTYCAD_API_TOKEN`.
-func NewDefaultClientFromEnv() (*Client, error) {
+func NewDefaultClientFromEnv() (*ClientWithResponses, error) {
 	token := os.Getenv(KITTYCAD_TOKEN_ENV_VAR)
 	if token == "" {
 		return nil, fmt.Errorf("The environment variable KITTYCAD_API_TOKEN must be set with your API token. Create a token at https://kittycad.io/account")
