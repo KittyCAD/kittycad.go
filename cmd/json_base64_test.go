c137b0ff8d5c88bcc2147d110380128a83d30e16
Jess Frazelle <github@jessfraz.com>
2022-07-20 17:14:28 -0700

    add base64
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

---
+++ cmd/json_base64_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBase64(t *testing.T) {
+	urlString := "aGVsbG8gd29ybGQK"
+	var jsonBase64 Base64
+	if err := jsonBase64.UnmarshalJSON([]byte(`"` + urlString + `"`)); err != nil {
+		t.Errorf("UnmarshalJSON failed: %v", err)
+	}
+}
+
+func TestBase64EmptyString(t *testing.T) {
+	urlString := ""
+	var jsonBase64 Base64
+	if err := jsonBase64.UnmarshalJSON([]byte(`"` + urlString + `"`)); err != nil {
+		t.Errorf("UnmarshalJSON failed: %v", err)
+	}
+}
+
+func TestBase64Null(t *testing.T) {
+	var jsonBase64 Base64
+	if err := jsonBase64.UnmarshalJSON([]byte("null")); err != nil {
+		t.Errorf("UnmarshalJSON failed: %v", err)
+	}
+
+	if err := jsonBase64.UnmarshalJSON(nil); err != nil {
+		t.Errorf("UnmarshalJSON failed: %v", err)
+	}
+}
---
+++ json_base64.go
@@ -0,0 +1,54 @@
+package kittycad
+
+import (
+	"bytes"
+	"encoding/base64"
+	"strings"
+)
+
+// Base64 is a wrapper around url.Base64 which marshals to and from empty strings.
+type Base64 struct {
+	Inner []byte
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (u Base64) MarshalJSON() ([]byte, error) {
+	if u.Inner == nil || len(u.Inner) <= 0 {
+		return []byte("null"), nil
+	}
+
+	return []byte(`"` + base64.StdEncoding.EncodeToString(u.Inner) + `"`), nil
+}
+
+func (u Base64) String() string {
+	if u.Inner == nil {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString(u.Inner)
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The time is expected to be a quoted string in RFC 3339 format.
+func (u *Base64) UnmarshalJSON(data []byte) (err error) {
+	// By convention, unmarshalers implement UnmarshalJSON([]byte("null")) as a no-op.
+	if bytes.Equal(data, []byte("null")) {
+		return nil
+	}
+
+	if bytes.Equal(data, []byte("")) {
+		return nil
+	}
+
+	if bytes.Equal(data, []byte(`""`)) {
+		return nil
+	}
+
+	// Fractional seconds are handled implicitly by Parse.
+	uu, err := base64.StdEncoding.DecodeString(strings.Trim(string(data), `"`))
+	if err != nil {
+		return err
+	}
+	*u = Base64{Inner: uu}
+	return
+}
